ef0e74d98fa803f3eecf5529ed4017b394e94c1c
VietHT <vitotec@gmail.com>
2013-01-30 17:27:18 +0700

    add try/catch

--- add_user.py
+++ add_user.py
@@ -20,28 +20,30 @@ l = ldap.open(IP)
 l.simple_bind_s("cn=admin,dc=ming,dc=vn","convit")
 
 
-for i in range(1680000, 2000001):
-
-    mail="viet%s@ming.vn" % (i)
-    
-    # The dn of our new entry/object
-    dn="mail=%s,ou=Users,domainName=ming.vn,o=domains,dc=ming,dc=vn" % (mail) 
-    
-    # A dict to help build the "body" of the object
-    attrs = {}
-    #attrs['objectclass'] = ['top','inetOrgPerson','shadowAccount','posixAccount']
-    attrs['objectclass'] = 'inetOrgPerson'
-    attrs['mail'] = mail
-    attrs['cn'] = 'Test'
-    attrs['sn'] = 'Test'
-    
-    # Convert our dict to nice syntax for the add-function using modlist-module
-    ldif = modlist.addModlist(attrs)
-    
-    # Do the actual synchronous add-operation to the ldapserver
-    l.add_s(dn,ldif)
-    print i
-    
+for i in range(1600000, 2000001):
+    try:
+        mail="viet%s@ming.vn" % (i)
+        
+        # The dn of our new entry/object
+        dn="mail=%s,ou=Users,domainName=ming.vn,o=domains,dc=ming,dc=vn" % (mail) 
+        
+        # A dict to help build the "body" of the object
+        attrs = {}
+        #attrs['objectclass'] = ['top','inetOrgPerson','shadowAccount','posixAccount']
+        attrs['objectclass'] = 'inetOrgPerson'
+        attrs['mail'] = mail
+        attrs['cn'] = 'Test'
+        attrs['sn'] = 'Test'
+        
+        # Convert our dict to nice syntax for the add-function using modlist-module
+        ldif = modlist.addModlist(attrs)
+        
+        # Do the actual synchronous add-operation to the ldapserver
+        l.add_s(dn,ldif)
+        print i
+    except Exception, e:
+        print e
+        
 # Its nice to the server to disconnect and free resources when done
 l.unbind_s()
 
