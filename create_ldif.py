d34f761e57cfcc5e38cad1c223f7d4fbbc134961
VietHT <vitotec@gmail.com>
2013-02-18 17:31:43 +0700

    hehe

---
+++ create_ldif.py
@@ -0,0 +1,14 @@
+file = open('/home/vietht/Desktop/users.ldif', 'w')
+
+MAX = 2000000
+
+for i in range(1, MAX + 1):
+    user = "viet%s" %(i)
+    file.write("dn: mail=%(user)s@ming.vn,ou=Users,domainName=ming.vn,o=domains,dc=ming,dc=vn\n\
+objectClass: inetOrgPerson\n\
+mail: %(user)s@ming.vn\n\
+cn: Test\n\
+sn: Test\n\n\
+" % {"user" : user})
+    
+file.close()
